9bdf1627352c236ea9a12d8af131331da80afba9
ganbarovrasul <93209831+ganbarovrasul@users.noreply.github.com>
2022-08-06 16:59:51 +0400

    Add files via upload

--- routers.py
+++ routers.py
@@ -1,14 +1,29 @@
-from flask import jsonify , request
+from time import sleep, time
+import requests
+from flask import jsonify, request
+from flask_cors import cross_origin
 from repostories import create_blog, get_blogs
 from app import app
+import time
 
-@app.route('/posts',methods = ['GET',"POST"])
+
+@app.route('/posts', methods = ['GET', "POST"])
+@cross_origin(supports_credentials=True)
 def posts():
     if request.method == 'POST':
         title = request.json['title']
+        
+        
         new_blog = create_blog(title=title)
+        post_data = {
+            'type': 'PostCreated',
+            'data': new_blog
+        }
+        requests.post('http://127.0.0.1:5003/events', json=post_data)
         return jsonify(new_blog), 201
+    time.sleep(1)    
     blogs = get_blogs()
     return jsonify(blogs)
+
 
 
